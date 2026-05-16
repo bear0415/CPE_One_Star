612389a8862b6b7cc77b6a6a7cddf195d15e9526
bear0415 <a1145514@mail.nuk.edu.tw>
2026-05-17 01:41:59 +0800

    Update UVa_10071.cpp

---
+++ Problem/UVa_10071.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int v,t;
+    long long ans;
+    while(cin>>v>>t){
+        ans=2*v*t;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
