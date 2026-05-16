d2aa5074d8b21932b33dc682118e25c6ecadd698
bear0415 <a1145514@mail.nuk.edu.tw>
2026-05-17 03:04:16 +0800

    Update UVa_10055.cpp

---
+++ Problem/UVa_10055.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long a,b;
+    long long ans;
+    while(cin>>a>>b){
+        ans=abs(a-b);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
